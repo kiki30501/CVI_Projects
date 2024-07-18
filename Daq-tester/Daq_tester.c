6b2876ca952b40ad4418504d70242feb7c1e5342
Reshef Schachter <sixsixbest@gmail.com>
2024-07-18 14:06:42 +0300

    finished reverse engineering the emulator

--- Daq-tester/Daq_tester.c
+++ Daq-tester/Daq_tester.c
@@ -6,6 +6,17 @@
 
 /*=============================================================================================*/
 
+// Peripheral Addresses:
+#define digital_out 	0x1 // Leds
+#define digital_in  	0x2 // Buttons
+#define analog_out_ch0  0x3 // Numeric Channel 0
+#define analog_in_ch0   0x4 // Knobs   Channel 0
+#define analog_out_ch1  0x5 // Numeric Channel 1
+#define analog_in_ch1   0x6 // Knobs   Channel 1
+
+/*=============================================================================================*/
+
+// Data structure to store the range of values for the analog channels
 typedef struct
 {
 	double l; // lower bound
@@ -25,13 +36,11 @@ range make_range(double lower, double upper)
 
 static int panelHandle;
 
-int my_com = 2;
+int my_com = 3;
 int Err;
+char SendBuff[3], RecvBuff[3], address;
 
-range analog0, analog1;
-analog0 = make_range(-10.0, 10.0);
-analog1 = make_range(-10.0, 10.0);
-
+range analog0, analog1;	// assigned values in main
 
 /*=============================================================================================*/
 
@@ -41,6 +50,11 @@ int main (int argc, char *argv[])
 		return -1;	/* out of memory */
 	if ((panelHandle = LoadPanel (0, "Daq_tester.uir", PANEL)) < 0)
 		return -1;
+
+	// Set analog ranges
+	analog0 = make_range(-10.0, 10.0);	// channel 0
+	analog1 = make_range(-10.0, 10.0);	// channel 1
+
 	DisplayPanel (panelHandle);
 	RunUserInterface ();
 	DiscardPanel (panelHandle);
@@ -67,15 +81,17 @@ int CVICALLBACK QuitCallback (int panel, int control, int event,
 /*=============================================================================================*/
 
 // This function is resposible to receive data from the DUT (emulator)
-// using a callback (instead of polling)
-void CVICALLBACK MyCallback (int portNumber, int eventMask, void *callbackData)
+// using a callback (instead of polling).
+// The function is used when getting data from the DUT after commanding it to return data.
+void CVICALLBACK tester_callback (int portNumber, int eventMask, void *callbackData)
 {
 	
 }
 
 /*=============================================================================================*/
 
-int CVICALLBACK Connect_COM (int panel, int control, int event,
+// Just a simple function to connect to the COM port to the tester.
+int CVICALLBACK connect_COM (int panel, int control, int event,
 							 void *callbackData, int eventData1, int eventData2)
 {
 	switch (event)
@@ -83,7 +99,7 @@ int CVICALLBACK Connect_COM (int panel, int control, int event,
 		case EVENT_COMMIT:
 			
 			OpenComConfig (my_com, "", 115200, 0, 8, 1, 512, 512);
-			
+			InstallComCallback (my_com, LWRS_RXFLAG, 0, 0, tester_callback, 0);
 
 			break;
 	}
@@ -92,8 +108,20 @@ int CVICALLBACK Connect_COM (int panel, int control, int event,
 
 /*=============================================================================================*/
 
-int CVICALLBACK Digital_Leds (int panel, int control, int event,
-							  void *callbackData, int eventData1, int eventData2)
+short format_short(short data)
+{
+	// Converts 12-bit data to 16-bit data but WITH sign extension for 16bit
+	// This is used as a function to prepare the data to be sent to the DUT
+
+	data <<= 4;
+	data >>= 4;
+	return data;
+}
+
+/*=============================================================================================*/
+
+int CVICALLBACK send_commands (int panel, int control, int event,
+							   void *callbackData, int eventData1, int eventData2)
 {
 	switch (event)
 	{
@@ -104,46 +132,38 @@ int CVICALLBACK Digital_Leds (int panel, int control, int event,
 	return 0;
 }
 
-/*=============================================================================================*/
 
-int CVICALLBACK Digital_Buttons (int panel, int control, int event,
-								 void *callbackData, int eventData1, int eventData2)
+
+
+// For the COM of tester (outgoing to the DUT)
+switch(address)
 {
-	switch (event)
-	{
-		case EVENT_COMMIT:
-
-			break;
-	}
-	return 0;
+	case digital_out:
+		
+		break;
+		
+	
+	case digital_in:
+		
+		break;
+		
+	
+	case analog_out_ch0:
+		
+		break;
+		
+	
+	case analog_in_ch0:
+		
+		break;
+		
+	
+	case analog_out_ch1:
+		
+		break;
+		
+	
+	case analog_in_ch1:
+		
+		break;
 }
-
-/*=============================================================================================*/
-
-int CVICALLBACK Analog_Numerics (int panel, int control, int event,
-								 void *callbackData, int eventData1, int eventData2)
-{
-	switch (event)
-	{
-		case EVENT_COMMIT:
-
-			break;
-	}
-	return 0;
-}
-
-/*=============================================================================================*/
-
-int CVICALLBACK Analog_Knobs (int panel, int control, int event,
-							  void *callbackData, int eventData1, int eventData2)
-{
-	switch (event)
-	{
-		case EVENT_COMMIT:
-
-			break;
-	}
-	return 0;
-}
-
-/*=============================================================================================*/
--- Daq-tester/Daq_tester.h
+++ Daq-tester/Daq_tester.h
@@ -24,18 +24,18 @@
 #define  PANEL_LED_1                      9       /* control type: LED, callback function: Digital_Leds */
 #define  PANEL_LED_7                      10      /* control type: LED, callback function: Digital_Leds */
 #define  PANEL_TEXT_0                     11      /* control type: textMsg, callback function: (none) */
-#define  PANEL_DIN_0                      12      /* control type: radioButton, callback function: Digital_Buttons */
-#define  PANEL_DIN_1                      13      /* control type: radioButton, callback function: Digital_Buttons */
-#define  PANEL_DIN_2                      14      /* control type: radioButton, callback function: Digital_Buttons */
-#define  PANEL_DIN_3                      15      /* control type: radioButton, callback function: Digital_Buttons */
-#define  PANEL_DIN_4                      16      /* control type: radioButton, callback function: Digital_Buttons */
-#define  PANEL_DIN_5                      17      /* control type: radioButton, callback function: Digital_Buttons */
-#define  PANEL_DIN_6                      18      /* control type: radioButton, callback function: Digital_Buttons */
-#define  PANEL_DIN_7                      19      /* control type: radioButton, callback function: Digital_Buttons */
+#define  PANEL_DIN_0                      12      /* control type: radioButton, callback function: send_commands */
+#define  PANEL_DIN_1                      13      /* control type: radioButton, callback function: send_commands */
+#define  PANEL_DIN_2                      14      /* control type: radioButton, callback function: send_commands */
+#define  PANEL_DIN_3                      15      /* control type: radioButton, callback function: send_commands */
+#define  PANEL_DIN_4                      16      /* control type: radioButton, callback function: send_commands */
+#define  PANEL_DIN_5                      17      /* control type: radioButton, callback function: send_commands */
+#define  PANEL_DIN_6                      18      /* control type: radioButton, callback function: send_commands */
+#define  PANEL_DIN_7                      19      /* control type: radioButton, callback function: send_commands */
 #define  PANEL_ANALOG_OUT_0               20      /* control type: numeric, callback function: Analog_Numerics */
 #define  PANEL_ANALOG_OUT_1               21      /* control type: numeric, callback function: Analog_Numerics */
-#define  PANEL_ANALOG_IN_0                22      /* control type: scale, callback function: Analog_Knobs */
-#define  PANEL_ANALOG_IN_1                23      /* control type: scale, callback function: Analog_Knobs */
+#define  PANEL_ANALOG_IN_0                22      /* control type: scale, callback function: send_commands */
+#define  PANEL_ANALOG_IN_1                23      /* control type: scale, callback function: send_commands */
 #define  PANEL_CONNECT                    24      /* control type: command, callback function: Connect_COM */
 #define  PANEL_TesterCOM                  25      /* control type: numeric, callback function: (none) */
 #define  PANEL_TEXT_2                     26      /* control type: textMsg, callback function: (none) */
@@ -55,12 +55,11 @@
 
      /* Callback Prototypes: */
 
-int  CVICALLBACK Analog_Knobs(int panel, int control, int event, void *callbackData, int eventData1, int eventData2);
 int  CVICALLBACK Analog_Numerics(int panel, int control, int event, void *callbackData, int eventData1, int eventData2);
 int  CVICALLBACK Connect_COM(int panel, int control, int event, void *callbackData, int eventData1, int eventData2);
-int  CVICALLBACK Digital_Buttons(int panel, int control, int event, void *callbackData, int eventData1, int eventData2);
 int  CVICALLBACK Digital_Leds(int panel, int control, int event, void *callbackData, int eventData1, int eventData2);
 int  CVICALLBACK QuitCallback(int panel, int control, int event, void *callbackData, int eventData1, int eventData2);
+int  CVICALLBACK send_commands(int panel, int control, int event, void *callbackData, int eventData1, int eventData2);
 
 
 #ifdef __cplusplus
