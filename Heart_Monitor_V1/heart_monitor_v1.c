a44fb0fc587253d9064ab11aa445bb2dee0da91b
Reshef Schachter <sixsixbest@gmail.com>
2024-08-15 18:19:58 +0300

    heart monitor - first commit

---
+++ Heart_Monitor_V1/heart_monitor_v1.c
@@ -0,0 +1,29 @@
+#include <cvirte.h>		
+#include <userint.h>
+#include "heart_monitor_v1.h"
+
+static int panelHandle;
+
+int main (int argc, char *argv[])
+{
+	if (InitCVIRTE (0, argv, 0) == 0)
+		return -1;	/* out of memory */
+	if ((panelHandle = LoadPanel (0, "heart_monitor_v1.uir", PANEL)) < 0)
+		return -1;
+	DisplayPanel (panelHandle);
+	RunUserInterface ();
+	DiscardPanel (panelHandle);
+	return 0;
+}
+
+int CVICALLBACK QuitCallback (int panel, int control, int event,
+							  void *callbackData, int eventData1, int eventData2)
+{
+	switch (event)
+	{
+		case EVENT_COMMIT:
+			QuitUserInterface (0);
+			break;
+	}
+	return 0;
+}
