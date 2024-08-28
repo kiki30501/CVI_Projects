e751d2be024bb5c512f1b58408dce46cfbcca64f
Reshef <reshef.schachter@s.afeka.ac.il>
2024-08-28 17:13:14 +0300

    Update for heart_monitor. saving progress before closing pc for now

--- Heart_Monitor_V1/heart_monitor_v1.c
+++ Heart_Monitor_V1/heart_monitor_v1.c
@@ -14,8 +14,10 @@
 
 /*  The frequency range of the heart rate and the BPF.
     These values were obtained from Application Note 223 By BIOPAC Systems, Inc.*/
-#define FREQ_MIN    0.5     // 30 bpm
-#define FREQ_MAX    35.0    // 2100 bpm
+#define FREQ_MIN    0.7
+#define FREQ_MAX    35.0
+#define FILTER_LEN	8000
+#define NUM_COEFF	10
 
 
 /*=============================================================================================*/
@@ -23,19 +25,24 @@
 static int panelHandle_main, panelHandle_emul, panelHandle_about;
 static int handle_tab_monitor, handle_tab_bpm;
 static int handle_dimmed_main, handle_dimmed_emul, handle_dimmed_emul_2;
+static int threadFunctionId_run;
 int Err, exiting;
-int run_emulator_thread, run_main_thread;
+int run_emulator_thread, run_main_thread, run_read_thread;
 int com_port_monitor = 3;
 int com_port_emulator = 4;
 char recv_buff[2];
 double Ts = 250e-6; // default value = 1/4000 seconds
+int    Fs = 4000;   // default value = 4000Hz
+double coeff[FILTER_LEN];
 double * monitor_waveform;
-int flag_calc_bpm;
-int lock;
+double * filtered_waveform;
 int time_window_size = 8000; // size of time window = 2 seconds * Byte rate; default value = 4000*2=8000
+double WF_gain = 1.0;
 
 wav_file emulator_wav;
 FILE *fp;
+
+static int CVICALLBACK read_from_com (void *functionData);
 /*=============================================================================================*/
 
 int main (int argc, char *argv[])
@@ -56,17 +63,26 @@ int main (int argc, char *argv[])
     handle_dimmed_emul = GetCtrlArrayFromResourceID   (panelHandle_emul, DIMMED_EMUL);
     handle_dimmed_emul_2 = GetCtrlArrayFromResourceID (panelHandle_emul, DIMMED_EMUL_2);
     
-    monitor_waveform = (double *) malloc (time_window_size * sizeof(double));
-    
-    CmtNewLock (NULL, 0, &lock);
+    monitor_waveform =  (double *) malloc (time_window_size * sizeof(double));
+    filtered_waveform = (double *) malloc (FILTER_LEN * sizeof(double));
+	
 
+    run_read_thread = 1;
+    CmtScheduleThreadPoolFunction (DEFAULT_THREAD_POOL_HANDLE,
+                        read_from_com, NULL, &threadFunctionId_run);
+	
+	
     DisplayPanel (panelHandle_main);
     RunUserInterface ();
+
+    CmtWaitForThreadPoolFunctionCompletion (DEFAULT_THREAD_POOL_HANDLE, threadFunctionId_run,
+                                                OPT_TP_PROCESS_EVENTS_WHILE_WAITING);
+    CmtReleaseThreadPoolFunctionID (DEFAULT_THREAD_POOL_HANDLE, threadFunctionId_run);
     
     // At the end of the program finish\close\free everything.
     free(monitor_waveform);
+    free(filtered_waveform);
     free_wav_data(emulator_wav);
-    CmtDiscardLock (lock);
     CloseCom(com_port_monitor);
     CloseCom(com_port_emulator);
     DiscardPanel (panelHandle_main);
@@ -83,12 +99,27 @@ int CVICALLBACK QuitCallback (int panel, int control, int event,
     switch (event)
     {
         case EVENT_COMMIT:
-            run_emulator_thread = 0;
-            run_main_thread = 0;
+            run_read_thread = 0;
+			run_main_thread = 0;
+			run_emulator_thread = 0;
+            
             QuitUserInterface (0);
             break;
     }
     return 0;
+}
+
+/*=============================================================================================*/
+int CVICALLBACK update_wf_gain (int panel, int control, int event,
+								void *callbackData, int eventData1, int eventData2)
+{
+	switch (event)
+	{
+		case EVENT_COMMIT:
+            GetCtrlVal (panel, control, &WF_gain);
+			break;
+	}
+	return 0;
 }
 
 /*=============================================================================================*/
@@ -157,7 +188,7 @@ int CVICALLBACK emulator_file_open (int panel, int control, int event,
             if (flag == 1)
                 free_wav_data(emulator_wav);
             SetCtrlVal (panel, PANEL_EMUL_TOGGLE_EMUL, 0);
-            SetCtrlVal (panel, PANEL_MAIN_TOGGLE_MAIN, 0);
+            //SetCtrlVal (panel, PANEL_MAIN_TOGGLE_MAIN, 0);
             
             FileSelectPopupEx ("", "*.wav", "*.wav", "Select test source waveform", VAL_LOAD_BUTTON, 0, 1, path);
             if (path[1] != ':')
@@ -173,19 +204,21 @@ int CVICALLBACK emulator_file_open (int panel, int control, int event,
             }
             // Read the wav file
             emulator_wav = get_wav_from_file(fp, get_wav_header_from_file(fp));
-            time_window_size = 2 * emulator_wav.header.sample_rate.int_value;
+            Fs = emulator_wav.header.sample_rate.int_value;
+            time_window_size = 2 * Fs;
             flag = 1;
             // Set the timer interval to the sample rate of the wav
-            Ts = 1.0/(double)(emulator_wav.header.sample_rate.int_value);
+            Ts = 1.0/(double)(Fs);
             // Set the duration of the file in seconds
             duration_file = emulator_wav.header.subchunk2_size.int_value / emulator_wav.header.byte_rate.int_value;
             sprintf(duration_file_string, "%02d:%02d:%02d", duration_file/3600, (duration_file%3600)/60, duration_file%60);
             SetCtrlVal (panel, PANEL_EMUL_DURATION, duration_file_string);
             SetCtrlArrayAttribute (handle_dimmed_emul_2, ATTR_DIMMED, 0);
             
-            //monitor_waveform = (short *) malloc (time_window_size * sizeof(short));
             monitor_waveform = (double *) realloc (monitor_waveform, time_window_size * sizeof(double));
             memset (monitor_waveform, 0, time_window_size);
+            filtered_waveform = (double *) realloc (filtered_waveform, FILTER_LEN * sizeof(double));
+            memset (filtered_waveform, 0, FILTER_LEN * sizeof(double));
             
             fclose(fp);
             break;
@@ -231,16 +264,20 @@ int CVICALLBACK quit_panel (int panel, int control, int event,
 
 static int CVICALLBACK emulator_tick (void *functionData)
 {
-    static int i;
+    //static int i;
+	int i;
     int k;
     char send_buff[2];
-    for (i=0; run_emulator_thread; i++)
+	int end = emulator_wav.header.subchunk2_size.int_value;
+    for (i=0; run_emulator_thread ; i++)
         {
-        k = i % emulator_wav.header.subchunk2_size.int_value;
+        k = i % end;
         memcpy(send_buff, emulator_wav.data[k].short_bytes, sizeof(short));
         ComWrt (com_port_emulator, send_buff, sizeof(short));
         Delay(Ts);
         }
+	//SetCtrlVal (panelHandle_emul, PANEL_EMUL_TOGGLE_EMUL, 0);
+	//run_emulator_thread = 0;
     return 0;
 }
 
@@ -273,30 +310,54 @@ int CVICALLBACK toggle_emulator (int panel, int control, int event,
 }
 
 /*=============================================================================================*/
+static int CVICALLBACK read_from_com (void *functionData)
+{
+    short temp;
+	while(run_read_thread)
+    {
+        if((Q = GetInQLen(com_port_monitor)) >= 2)
+	            ComRd (com_port_monitor, recv_buff, sizeof(short));
+	        else
+	            memset(recv_buff, 0, sizeof(short));
+	        memcpy (&temp, recv_buff, 2);
+	        Shift (monitor_waveform, time_window_size, -1, monitor_waveform);
+	            
+	        monitor_waveform[time_window_size - 1] = (double)(temp);
+
+	        // Apply the band pass filter
+			WindFIR_Filtering ((&monitor_waveform[time_window_size - (FILTER_LEN - NUM_COEFF-1)]), (FILTER_LEN - NUM_COEFF-1), NUM_COEFF, BANDPASS, Fs, FREQ_MIN, FREQ_MAX, HAMMING____, 0.0,
+						   filtered_waveform);
+    }
+	return 0;
+}
+
+/*=============================================================================================*/
 static int CVICALLBACK monitor_graph_tick (void *functionData)
 {
     static int m;
     int n;
     int Q;
     short temp;
-    
+    memset (monitor_waveform, 0, sizeof(monitor_waveform));
     for (m=0; run_main_thread; m++)
         {
-        n = (m < time_window_size) ? m : time_window_size - 1;
-        if ((m >= time_window_size) && (m % (time_window_size / 4) == 0))
-            flag_calc_bpm = 1;
-        if((Q = GetInQLen(com_port_monitor)) >= 2)
-            ComRd (com_port_monitor, recv_buff, sizeof(short));
-        else
-            memset(recv_buff, 0, sizeof(short));
-        memcpy (&temp, recv_buff, 2);
-        if (!(m < time_window_size))
-            Shift (monitor_waveform, time_window_size, -1, monitor_waveform);
-            
-        monitor_waveform[n] = (double)(temp);
-        
-        PlotStripChartPoint (handle_tab_monitor, TAB_MONITO_STRIPCHART, monitor_waveform[n]);
-        Delay(Ts);
+	        // n = (m < time_window_size) ? m : time_window_size - 1;
+	        // if((Q = GetInQLen(com_port_monitor)) >= 2)
+	        //     ComRd (com_port_monitor, recv_buff, sizeof(short));
+	        // else
+	        //     memset(recv_buff, 0, sizeof(short));
+	        // memcpy (&temp, recv_buff, 2);
+	        
+	        // Shift (monitor_waveform, time_window_size, -1, monitor_waveform);
+	            
+	        // monitor_waveform[time_window_size - 1] = (double)(temp);
+
+	        // // Apply the band pass filter
+			// WindFIR_Filtering ((&monitor_waveform[time_window_size - (FILTER_LEN - NUM_COEFF-1)]), (FILTER_LEN - NUM_COEFF-1), NUM_COEFF, BANDPASS, Fs, FREQ_MIN, FREQ_MAX, HAMMING____, 0.0,
+			// 			   filtered_waveform);
+			
+			PlotStripChartPoint (handle_tab_monitor, TAB_MONITO_STRIPCHART, WF_gain * filtered_waveform[FILTER_LEN - NUM_COEFF-1]);
+	        Delay(Ts);
         }
     return 0;
 }
@@ -310,14 +371,20 @@ static int CVICALLBACK bpm_graph_tick (void *functionData)
         
         }*/
     double bpm, phase, amp;
-    if(flag_calc_bpm)
+    while (run_main_thread)
     {
         SearchType freq_search;
         freq_search.centerFrequency = (FREQ_MIN + FREQ_MAX)/2.0;
         freq_search.frequencyWidth  = FREQ_MAX - FREQ_MIN;
-        SingleToneInfo (monitor_waveform, time_window_size, Ts, &freq_search, &bpm, &amp, &phase);
-        SetCtrlVal (panelHandle_main, PANEL_MAIN_NUMERIC_BPM, (short)bpm);
-        flag_calc_bpm = 0;
+		//Bw_BPF (Convolve (coef, FILTER_LEN, (monitor_waveform + time_window_size - FILTER_LEN), time_window_size - FILTER_LEN, filtered_waveform), FILTER_LEN, Fs, FREQ_MIN, FREQ_MAX, NUM_COEFF, filtered_waveform);
+		//Convolve (coeff, FILTER_LEN, (monitor_waveform + time_window_size - FILTER_LEN), FILTER_LEN, filtered_waveform);
+		/*WindFIR_Filtering ((&monitor_waveform[time_window_size - (FILTER_LEN - NUM_COEFF-1)]), (FILTER_LEN - NUM_COEFF-1), NUM_COEFF, BANDPASS, Fs, FREQ_MIN, FREQ_MAX, HAMMING____, 0.0,
+						   filtered_waveform);*/
+		
+        SingleToneInfo (filtered_waveform, FILTER_LEN, Ts, &freq_search, &bpm, &amp, &phase);
+		//SingleToneInfo (filtered_waveform, FILTER_LEN, Ts, NULL, &bpm, &amp, &phase);
+        SetCtrlVal (panelHandle_main, PANEL_MAIN_NUMERIC_BPM, bpm);
+        Delay(1.0);
     }
     return 0;
 }
@@ -326,7 +393,7 @@ static int CVICALLBACK bpm_graph_tick (void *functionData)
 int CVICALLBACK toggle_monitor (int panel, int control, int event,
         void *callbackData, int eventData1, int eventData2)
 {
-    static int threadFunctionId;
+    static int threadFunctionId_A, threadFunctionId_B;
     switch (event)
         {
         case EVENT_VAL_CHANGED:
@@ -334,25 +401,24 @@ int CVICALLBACK toggle_monitor (int panel, int control, int event,
             
             if(run_main_thread)
             {
-                threadFunctionId = 1;
+                threadFunctionId_A = 1;
                 CmtScheduleThreadPoolFunction (DEFAULT_THREAD_POOL_HANDLE,
-                                    monitor_graph_tick, NULL, &threadFunctionId);
-                threadFunctionId = 2;
+                                    monitor_graph_tick, NULL, &threadFunctionId_A);
+                threadFunctionId_B = 2;
                 CmtScheduleThreadPoolFunction (DEFAULT_THREAD_POOL_HANDLE,
-                                    bpm_graph_tick, NULL, &threadFunctionId);
+                                    bpm_graph_tick, NULL, &threadFunctionId_B);
             }
             else
             {
-                threadFunctionId = 1;
                 CmtWaitForThreadPoolFunctionCompletion (DEFAULT_THREAD_POOL_HANDLE,
-                                                threadFunctionId,
+                                                threadFunctionId_A,
                                                 OPT_TP_PROCESS_EVENTS_WHILE_WAITING);
-                CmtReleaseThreadPoolFunctionID (DEFAULT_THREAD_POOL_HANDLE, threadFunctionId);
-                threadFunctionId = 2;
+                CmtReleaseThreadPoolFunctionID (DEFAULT_THREAD_POOL_HANDLE, threadFunctionId_A);
+                
                 CmtWaitForThreadPoolFunctionCompletion (DEFAULT_THREAD_POOL_HANDLE,
-                                                threadFunctionId,
+                                                threadFunctionId_B,
                                                 OPT_TP_PROCESS_EVENTS_WHILE_WAITING);
-                CmtReleaseThreadPoolFunctionID (DEFAULT_THREAD_POOL_HANDLE, threadFunctionId);
+                CmtReleaseThreadPoolFunctionID (DEFAULT_THREAD_POOL_HANDLE, threadFunctionId_B);
             }
             break;
         }
--- Heart_Monitor_V1/heart_monitor_v1.h
+++ Heart_Monitor_V1/heart_monitor_v1.h
@@ -29,11 +29,12 @@
 #define  PANEL_MAIN_TAB_GRAPHS            3       /* control type: tab, callback function: (none) */
 #define  PANEL_MAIN_NUMERIC_BPM           4       /* control type: numeric, callback function: (none) */
 #define  PANEL_MAIN_BTN_CONNECT_MONITOR   5       /* control type: command, callback function: com_control */
-#define  PANEL_MAIN_NUMERIC_COM_MONITOR   6       /* control type: numeric, callback function: (none) */
-#define  PANEL_MAIN_DECORATION_2          7       /* control type: deco, callback function: (none) */
-#define  PANEL_MAIN_DECORATION_3          8       /* control type: deco, callback function: (none) */
-#define  PANEL_MAIN_DECORATION            9       /* control type: deco, callback function: (none) */
-#define  PANEL_MAIN_TOGGLE_MAIN           10      /* control type: binary, callback function: toggle_monitor */
+#define  PANEL_MAIN_WF_GAIN               6       /* control type: numeric, callback function: update_wf_gain */
+#define  PANEL_MAIN_NUMERIC_COM_MONITOR   7       /* control type: numeric, callback function: (none) */
+#define  PANEL_MAIN_DECORATION_2          8       /* control type: deco, callback function: (none) */
+#define  PANEL_MAIN_DECORATION_3          9       /* control type: deco, callback function: (none) */
+#define  PANEL_MAIN_DECORATION            10      /* control type: deco, callback function: (none) */
+#define  PANEL_MAIN_TOGGLE_MAIN           11      /* control type: binary, callback function: toggle_monitor */
 
 #define  PNL_ABOUT                        3
 #define  PNL_ABOUT_QUITBUTTON             2       /* control type: command, callback function: quit_panel */
@@ -70,6 +71,7 @@ int  CVICALLBACK quit_panel(int panel, int control, int event, void *callbackDat
 int  CVICALLBACK QuitCallback(int panel, int control, int event, void *callbackData, int eventData1, int eventData2);
 int  CVICALLBACK toggle_emulator(int panel, int control, int event, void *callbackData, int eventData1, int eventData2);
 int  CVICALLBACK toggle_monitor(int panel, int control, int event, void *callbackData, int eventData1, int eventData2);
+int  CVICALLBACK update_wf_gain(int panel, int control, int event, void *callbackData, int eventData1, int eventData2);
 
 
 #ifdef __cplusplus
